9e98f19979224a3875d52665fb37805aef396864
Dean Kago <martokhago@gmail.com>
2022-03-06 17:24:34 +0300

    added picked function

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ let MM = month.value;
 let CC = year.value.slice(0, 2);
 let YY = year.value.substring(2);
 
-Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
+return d.toFixed
 }
+// 
+function picked(){
+let gender = document.getElementById("radio")
+} 
