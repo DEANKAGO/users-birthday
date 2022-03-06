0b0a296d92eaaab97cc1edb6c190894a5d8bc4a7
Dean Kago <martokhago@gmail.com>
2022-03-06 17:08:15 +0300

    added formula for calculating birthday

--- script.js
+++ script.js
@@ -8,4 +8,5 @@ let MM = month.value;
 let CC = year.value.slice(0, 2);
 let YY = year.value.substring(2);
 
+Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 }
