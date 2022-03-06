809e5db42482232ddc93631553522015c5ccb859
Dean Kago <martokhago@gmail.com>
2022-03-06 23:44:45 +0300

    added alert to display Akan name

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-let year = document.getElementById("year");
-let month = document.getElementById("month");
-let date = document.getElementById("date");
-// 
 function calculate(){
-let DD = day.value;
-let MM = month.value;
-let CC = year.value.slice(0, 2);
-let YY = year.value.substring(2);
+  let longDate = document.getElementById("date").value
+  let year = longDate.substring(0,4)
+  let month = longDate.substring(5,7)
+  let date = longDate.substring(8,10)
+  
+
+let DD = parseInt(date)
+let MM = parseInt(month)
+let CC = parseInt(year.substring(0,2))
+let YY = parseInt(year.substring(2,4));
 
 let d = Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
-return d.toFixed
+return d
 }
 // 
 function picked(){
-let gender = document.getElementById("radio");
+let gender = document.querySelector('input[name="Male-Female"]:checked').value;
 let index = calculate();
 let male =["Kwasi", "Kwadwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwake"];
 let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 // 
 // 
+let Akan
+let day
 if (gender.value == "male"){
-  let Akan = male[index]
+  Akan = male[index]
+  day = weekdays[index]
+  alert("You were born on " + day + ". Your Akan name is " + Akan)
 } else{
-  let Akan = female[index]
+  Akan = female[index]
+  day = weekdays[index]
+  alert("You were born on " + day + ". Your Akan name is " + Akan)
 }
 } 
