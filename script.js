47fe95d3064dba041e78c7076027429d5c8dd8b6
Dean Kago <martokhago@gmail.com>
2022-03-06 22:13:12 +0300

    added query selector

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ let MM = month.value;
 let CC = year.value.slice(0, 2);
 let YY = year.value.substring(2);
 
-let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
+let d = Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
 return d.toFixed
 }
 // 
 function picked(){
-let gender = document.getElementById("radio")
+let gender = document.getElementById("radio");
+let index = calculate();
+let male =["Kwasi", "Kwadwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwake"];
+let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+// 
+// 
+if (gender.value == "male"){
+  let Akan = male[index]
+} else{
+  let Akan = female[index]
+}
 } 
