b400bf2ab1fd7f078b5a0d6508811ced920a54ab
Dean Kago <martokhago@gmail.com>
2022-03-06 16:11:13 +0300

    added calculate function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let year = document.getElementById("year");
 let month = document.getElementById("month");
-let date = document.getElementById("date")
+let date = document.getElementById("date");
+// 
+function calculate(){
+let DD = day.value;
+let MM = month.value;
+
+
+}
