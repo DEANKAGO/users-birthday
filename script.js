b323cb4135b9ee7a70d5d6534785b5ef8bb7a176
Dean Kago <martokhago@gmail.com>
2022-03-07 19:31:40 +0300

    added font family

--- script.js
+++ script.js
@@ -12,25 +12,38 @@ let YY = parseInt(year.substring(2,4));
 
 let d = Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
 return d
+
+
+
 }
+
 // 
 function picked(){
 let gender = document.querySelector('input[name="Male-Female"]:checked').value;
 let index = calculate();
-let male =["Kwasi", "Kwadwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwake"];
+let male = ["Kwasi", "Kwadwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwake"];
 let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
-let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-// 
-// 
-let Akan
-let day
-if (gender.value == "male"){
-  Akan = male[index]
-  day = weekdays[index]
-  alert("You were born on " + day + ". Your Akan name is " + Akan)
-} else{
-  Akan = female[index]
-  day = weekdays[index]
-  alert("You were born on " + day + ". Your Akan name is " + Akan)
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+if (date.value ==(date<=0 || date>31)) {
+  alert("Please enter a valid date");
+}else{
+  let Akan
+  let day
+  if (gender == "male"){
+    Akan = male[index]
+    day = weekdays[index]
+    alert("You were born on " + day + ". Your Akan name is " + Akan)
+  } else{ 
+    Akan = female[index]
+    day = weekdays[index]
+    alert("You were born on " + day + ". Your Akan name is " + Akan)
+  }
+  } alert("Thank you");
 }
-} 
+
+
+
+
+
